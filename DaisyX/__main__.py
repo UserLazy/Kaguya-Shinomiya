81ed790fc0985b74690f9f3d1d860ac2a1bbd8e1
Feri <87620783+FeriEXP@users.noreply.github.com>
2021-08-23 13:57:55 +0700

    add bokep

--- DaisyX/__main__.py
+++ DaisyX/__main__.py
@@ -62,6 +62,32 @@ from DaisyX.modules.helper_funcs.chat_status import is_user_admin
 from DaisyX.modules.helper_funcs.misc import paginate_modules
 from DaisyX.modules.helper_funcs.readable_time import get_readable_time
 
+
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+
+    return ping_time
+
+
 PM_START_TEXT = """
 ──「 [Kaguya Shinomiya](https://telegra.ph/file/18cbe40364ec9befb73c4.jpg) 」──
 
